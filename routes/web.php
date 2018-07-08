41e08ee7d8631c79815ddc48de3f957a030c2c66
vagrant <vagrant@homestead>
2018-07-08 12:10:49 +0000

    收货地址列表.

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::group(['middleware' => 'email_verified'], function() {
-        // 删掉这里面的内容
+        Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     });
 });
