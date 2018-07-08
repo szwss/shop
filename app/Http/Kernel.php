b738bee657f7addc13498e40b4f25c21220c02f2
vagrant <vagrant@homestead>
2018-07-08 10:24:57 +0000

    邮箱验证中间件

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        //20180708邮箱验证中间件
+        'email_verified' => \App\Http\Middleware\CheckIfEmailVerified::class,
     ];
 }
