f79fb7b6c5ea469289f7df24267ce8c391f80e4d
vagrant <vagrant@homestead>
2018-07-09 08:33:28 +0000

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -32,17 +32,16 @@ Route::group(['middleware' => 'auth'], function() {
         Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 
-
-
         Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 
-
         Route::post('cart', 'CartController@add')->name('cart.add');
         Route::get('cart', 'CartController@index')->name('cart.index');
         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+
+        Route::post('orders', 'OrdersController@store')->name('orders.store');
     });
 });
 Route::get('products', 'ProductsController@index')->name('products.index');
