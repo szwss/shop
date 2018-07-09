5c3fed1bf697ab014b8b8bda6fc9de3875723fc0
vagrant <vagrant@homestead>
2018-07-09 03:38:09 +0000

    购物车页面

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
 
-
+use App\Models\ProductSku;
 class CartController extends Controller
 {
     public function add(AddCartRequest $request)
@@ -32,6 +32,20 @@ class CartController extends Controller
             $cart->save();
         }
 
+
+        return [];
+    }
+
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+
+        return view('cart.index', ['cartItems' => $cartItems]);
+    }
+
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
 
         return [];
     }
--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,8 @@ Route::group(['middleware' => 'auth'], function() {
 
 
         Route::post('cart', 'CartController@add')->name('cart.add');
+        Route::get('cart', 'CartController@index')->name('cart.index');
+        Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
     });
 });
 Route::get('products', 'ProductsController@index')->name('products.index');
