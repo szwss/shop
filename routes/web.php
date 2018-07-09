e0eea04c2482bd3b223525fd5ed180f19a892ccf
vagrant <vagrant@homestead>
2018-07-09 02:11:32 +0000

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,9 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
         Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+
+
     });
 });
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
