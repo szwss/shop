b857de7503fe5c1e25ed6726ad2c1c353abfcbb5
vagrant <vagrant@homestead>
2018-07-10 03:01:22 +0000

    用户界面 - 申请退款

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,8 @@ Route::group(['middleware' => 'auth'], function() {
 
         Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
         Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
+
+        Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
     });
 });
 //服务器端回调的路由不能放到带有 auth 中间件的路由组中，因为支付宝的服务器请求不会带有认证信息。
