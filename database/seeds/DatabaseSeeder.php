0c35c8fe0e9cd44ac7b40db19049a529c1a99f57
vagrant <vagrant@homestead>
2018-07-10 06:26:16 +0000

    完善假数据填充

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        //接下来我们要把刚刚的这些 Seeder 文件注册到 DatabaseSeeder 中：
+        $this->call(UsersSeeder::class);
+        $this->call(UserAddressesSeeder::class);
+        $this->call(ProductsSeeder::class);
+        $this->call(CouponCodesSeeder::class);
+        $this->call(OrdersSeeder::class);
     }
 }
